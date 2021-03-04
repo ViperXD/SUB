b2d594c9a0d79dbfaaaefcf3cb380a3b67f87035
Fayas <76828314+FayasNoushad@users.noreply.github.com>
2021-03-04 18:10:44 +0530

    Update messages.py

--- messages.py
+++ messages.py
@@ -2,7 +2,7 @@ class Messages(object):
   START = """
 <b>Hello <a href='tg://user?id={}'>{}</a>, I' am subtitle downloader bot. I can provide movie subtitles.</b>
 
-/help for more...
+/help and /about for more...
 """
   HELP = """
 - Just send me Movie name. Use @imdb or @imdbot inline to get correct movie name.
