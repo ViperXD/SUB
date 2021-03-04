6d0dac5e29f7400b551a3bd0e18a160a98b93a15
Fayas <76828314+FayasNoushad@users.noreply.github.com>
2021-03-04 16:50:08 +0530

    Create messages.py

---
+++ messages.py
@@ -0,0 +1,6 @@
+class Messages(object):
+  START_TEXT = """
+<b>Hello {}, I' am subtitle downloader bot. I can provide movie subtitles.
+
+==> Just send me Movie name. Use @imdb or @imdbot inline to get currect movie name.</b>
+"""
