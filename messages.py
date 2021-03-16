f815e0977a3dd39f04b1f80d270a7251f5ede930
Fayas <76828314+FayasNoushad@users.noreply.github.com>
2021-03-16 18:16:04 +0530

    Update messages.py

--- messages.py
+++ messages.py
@@ -2,7 +2,7 @@ class Messages(object):
   START = """
 Hello <a href='tg://user?id={}'>{}</a>, I' am subtitle downloader bot. I can provide movie subtitles.
 
-- Just send me Movie name. Use @imdb or @imdbot inline to get correct movie name.
+- Just send me Movie name. Use @imdb or @imdbot inline to get correct movie name
 - I will search that subtitle
 - Then select the language
 - Then I will send that subtitle file to you 
