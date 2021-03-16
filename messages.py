67b4c57e655aad048b9dcf5e1ba1b61c84279a6e
Fayas <76828314+FayasNoushad@users.noreply.github.com>
2021-03-16 10:22:12 +0530

    Update messages.py

--- messages.py
+++ messages.py
@@ -1,22 +1,14 @@
 class Messages(object):
   START = """
-<b>Hello <a href='tg://user?id={}'>{}</a>, I' am subtitle downloader bot. I can provide movie subtitles.
+Hello <a href='tg://user?id={}'>{}</a>, I' am subtitle downloader bot. I can provide movie subtitles.
 
-Made with love ❤️ by @FayasNoushad from India 🇮🇳. Use /help and /about for more...</b>
+Made by @FayasNoushad
 """
   HELP = """
 - Just send me Movie name. Use @imdb or @imdbot inline to get correct movie name.
 - I will search that subtitle
 - Then select the language
-- Then I will send that subtitle file to you
-"""
-  ABOUT = """
-- Name : <a href='https://telegram.me/FnSubtitleBot'>Subtitle Bot</a>
-- Channel : <a href='http://telegram.me/FayasNoushad'>Fayas</a>
-- Support : <a href='https://telegram.me/FayasChat'>Fayas Chat</a>
-- Projects : <a href='http://telegram.me/FNPROJECTS'>Fayas Projects</a>
-- Language : <a href='https://www.python.org/'>Python3</a>
-- Framework : <a href='https://python-telegram-bot.org'>Python Telegram Bot</a>
-- Server : <a href='https://heroku.com/'>Heroku</a>
-- Source : <a href='https://github.com/FayasNoushad/SubtitlesBot'>Click here</a>
+- Then I will send that subtitle file to you 
+
+Made by @FayasNoushad
 """
